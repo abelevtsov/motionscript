247767e4eb9ec81d4ec411f946c1eda7d9dc6d2a
abelevtsov <belevtsov80@mail.ru>
2016-01-23 16:25:09 +0300

    added motionscript solution with first component ScriptEditor (see visio schema)

---
+++ src/OwnSpace.MotionScript.ScriptEditor/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace OwnSpace.MotionScript.ScriptEditor
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = 
+                _ =>
+                {
+                    return View["index"];
+                };
+        }
+    }
+}
