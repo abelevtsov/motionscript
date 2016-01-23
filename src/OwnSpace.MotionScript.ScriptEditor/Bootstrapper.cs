f15df1498b7c868b70919f68d950f7e4e3a29c68
abelevtsov <belevtsov80@mail.ru>
2016-01-23 17:50:58 +0300

    added mandatory components (loging, IoC, e.t.c)

--- src/OwnSpace.MotionScript.ScriptEditor/Bootstrapper.cs
+++ src/OwnSpace.MotionScript.ScriptEditor/Bootstrapper.cs
@@ -1,8 +1,8 @@
-﻿using Nancy;
+﻿using Nancy.Bootstrappers.Windsor;
 
 namespace OwnSpace.MotionScript.ScriptEditor
 {
-    public class Bootstrapper : DefaultNancyBootstrapper
+    public class Bootstrapper : WindsorNancyBootstrapper
     {
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
