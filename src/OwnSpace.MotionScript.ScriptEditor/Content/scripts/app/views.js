705a8336e1a5986dc75ead477b62fc3322357c6f
abelevtsov <belevtsov80@mail.ru>
2016-04-16 16:20:06 +0300

    scenes add/delete implemented

--- src/OwnSpace.MotionScript.ScriptEditor/Content/scripts/app/views.js
+++ src/OwnSpace.MotionScript.ScriptEditor/Content/scripts/app/views.js
@@ -1,20 +1,27 @@
 ﻿define(["marionette", "underscore", "templates", "models", "jquery", "scenarioflow"], function (Marionette, _, templates, Models, $, scenarioflow) {
     var createCaretPlacer = function(atStart) {
-            return function(el) {
-                el.focus();
-                if (window.getSelection && document.createRange) {
+            if (window.getSelection && document.createRange) {
+                return function(el) {
+                    el.focus();
                     var range = document.createRange();
                     range.selectNodeContents(el);
                     range.collapse(atStart);
                     var sel = window.getSelection();
                     sel.removeAllRanges();
                     sel.addRange(range);
-                } else if (document.body.createTextRange) {
+                };
+            } else if (document.body.createTextRange) {
+                return function(el) {
+                    el.focus();
                     var textRange = document.body.createTextRange();
                     textRange.moveToElementText(el);
                     textRange.collapse(atStart);
                     textRange.select();
-                }
+                };
+            }
+
+            return function() {
+
             };
         },
         placeCaretAtEnd = createCaretPlacer(false),
@@ -46,6 +53,7 @@
         }),
         NavView = Marionette.ItemView.extend({
             template: _.template(templates.nav),
+            tagName: "ul",
             events: {
                 "click .scenenav-list-link": "navigate"
             },
@@ -100,6 +108,7 @@
             template: _.template(templates.block),
             tagName: "block",
             events: {
+                "click .clear": "clearBlock", // ToDo: add "X" button to the right of block for this purposes
                 "focusin p.block": "setActive"
             },
             initialize: function(options) {
@@ -120,6 +129,9 @@
                 this.vent.trigger("scenario:resetActive");
                 model.set({ active: true });
                 this.vent.trigger("header:setAction", model.get("type"));
+            },
+            clearBlock: function(e) {
+                $(e.target).closest("p.block").text("").focus();
             }
         }),
         SceneView = Marionette.CompositeView.extend({
@@ -132,8 +144,7 @@
                 }
             },
             events: {
-                "click .delete": "deleteScene", // ToDo: add "X" button to the right of block for this purposes
-                "keypress p.block": "addNew",
+                "keypress p.block": "addNewBlock",
                 "keyup p.block": "processBlock"
             },
             initialize: function(options) {
@@ -148,18 +159,19 @@
                 this.listenTo(this.collection, "reset", this.render);
             },
             renderBlock: function() {
-                console.log("SceneView:renderBlock");
+                console.log("SceneView: renderBlock");
             },
             getNextType: function(haveText, type) {
                 var flow = haveText ? scenarioflow.text : scenarioflow.empty;
 
                 return flow[type];
             },
-            addNew: function(e) {
+            addNewBlock: function(e) {
                 var enterKey = 13,
                     collection = this.collection,
                     activeBlock = collection.findWhere("active"),
                     activeBlockIndex = collection.indexOf(activeBlock),
+                    $activeView,
                     $current = $(e.target),
                     $block = $current.parent(),
                     $prev = $block.prev();
@@ -172,21 +184,44 @@
                             type: nextType.next
                         };
 
+                    // ToDo: refact this shit
+                    if (data.type === "sceneheading") {
+                        var blocksToMove = collection.slice(activeBlockIndex + 1, collection.length);
+                        if (nextType.inPlace) {
+                            collection.remove(collection.at(activeBlockIndex));
+                            $current = $prev;
+                        } else {
+                            $current = $block;
+                        }
+
+                        collection.remove(blocksToMove);
+                        this.vent.trigger("scenario:addScene", data, this.model, blocksToMove);
+                        $activeView = $current.closest("scene").next().find("p.block.sceneheading");
+                        $activeView.focus();
+                        placeCaretAtEnd($activeView[0]);
+                        return;
+                    }
+
                     var model = new Models.ScriptBlock(data);
                     if (nextType.inPlace) {
                         collection.remove(collection.at(activeBlockIndex));
 
                         collection.add(model, { at: activeBlockIndex });
                         this.vent.trigger("header:setAction", model.get("type"));
-                        $prev.next().find("p.block").focus();
+                        $current = $prev;
                     } else {
                         collection.add(model, { at: activeBlockIndex + 1 });
-                        $block.next().find("p.block").focus();
+                        $current = $block;
                     }
+
+                    $activeView = $current.next().find("p.block");
+                    $activeView.focus();
+                    placeCaretAtEnd($activeView[0]);
                 }
             },
             processBlock: function(e) {
                 var backspaceKey = 8,
+                    removeKey = "readyForRemove",
                     collection = this.collection,
                     activeBlock = collection.findWhere("active"),
                     activeBlockIndex = collection.indexOf(activeBlock),
@@ -194,28 +229,47 @@
 
                 if (e.which === backspaceKey) {
                     if ($current.text()) {
+                        $current.data(removeKey, false);
                         return;
                     }
 
-                    var $prev = $current.parent().prev().find("p.block");
+                    if (!$current.data(removeKey)) {
+                        $current.data(removeKey, true);
+                        return;
+                    }
+
+                    var type = activeBlock.get("type"),
+                        $prev = type === "sceneheading" ? $current.closest("scene").prev().find("p.block").last() : $current.parent().prev().find("p.block");
 
                     collection.remove(activeBlock);
+                    if (type === "sceneheading") { // ToDo: use !collection.length and move blocks to prev scene otherwise
+                        this.deleteScene();
+                        $prev.focus();
+                        if ($prev.length && $prev.text()) {
+                            placeCaretAtEnd($prev[0]);
+                        }
+
+                        return;
+                    }
+
                     activeBlock = collection.at(activeBlockIndex - 1);
-                    activeBlock.set({ active: true });
+                    activeBlock && activeBlock.set({ active: true });
                     $prev.focus();
                     if ($prev.length) {
                         placeCaretAtEnd($prev[0]);
                     }
 
-                    this.vent.trigger("header:setAction", activeBlock.get("type"));
+                    activeBlock && this.vent.trigger("header:setAction", activeBlock.get("type"));
                 } else {
+                    $current.data(removeKey, false);
                     activeBlock.set({ text: $current.text() });
                 }
             },
             deleteScene: function() {
                 this.model.destroy();
-                this.collection.clear();
+                this.collection.reset();
                 this.remove();
+                this.vent.trigger("scenario:removeScene", this.model);
             }
         }),
         ScenarioView = Marionette.CompositeView.extend({
@@ -227,26 +281,33 @@
                     vent: this.options.vent
                 }
             },
-            events: {
-                "click #add": "addScene"
-            },
-            addScene: function(e) {
-                e.preventDefault();
-
-                var data = {};
-
-                this.collection.add(new Models.Scene(data));
-            },
             initialize: function(options) {
                 if (options) {
                     if (options.vent) {
                         this.vent = options.vent;
                         this.vent.on("scenario:resetActive", this.resetActive, this);
+                        this.vent.on("scenario:addScene", this.addScene, this);
+                        this.vent.on("scenario:removeScene", this.removeScene, this);
                     }
                 }
 
                 this.listenTo(this.collection, "add", this.renderScene);
                 this.listenTo(this.collection, "reset", this.render);
+            },
+            addScene: function(data, currentScene, blocksToMove) {
+                var collection = this.collection,
+                    headingBlock = new Models.ScriptBlock({ type: data.type, text: "SCENE " + ++collection.length }),
+                    currentSceneIndex = collection.indexOf(currentScene);
+
+                blocksToMove.unshift(headingBlock);
+                var scene = new Models.Scene({
+                        blocks: new Models.ScriptBlocks(blocksToMove)
+                    });
+
+                collection.add(scene, { at: currentSceneIndex + 1 });
+            },
+            removeScene: function(scene) {
+                this.collection.remove(scene);
             },
             resetActive: function() {
                 var activeBlock = this.model.getActiveBlock();
@@ -257,7 +318,7 @@
                 }
             },
             renderScene: function(scene) {
-                console.log("renderScene" + scene.toJSON());
+                console.log("renderScene: " + scene.toJSON());
             }
         }),
         MainView = Marionette.LayoutView.extend({
